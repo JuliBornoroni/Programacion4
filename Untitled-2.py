bcf7d38d9356c7189825808a1f52e1c965a1a490
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:34:36 -0300

    mi primer commit

---
+++ Untitled-2.py
@@ -0,0 +1,10 @@
+
+#Tabla de multiplicar
+a=1
+While a!=o:
+    a=int(input("Dame el valor y te devuelvo la tabla de multiplicar"))
+    
+    for i in range (1,11)
+        print(f"(i)x(a)=")
+        print(i*a)
+print("programa finalizado")
