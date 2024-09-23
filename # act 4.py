f0962ff075651f395ef30ba02d16ee4b69fa5fb4
JuliBornoroni <juliborno747@gmail.com>
2024-09-23 15:34:41 -0300

    Ejercicios de trabajo practico 7 2309

---
+++ # act 4.py
@@ -0,0 +1,12 @@
+# act 4
+
+res = 4
+inten = 3
+volt = inten * res
+print(volt)
+## el ejercicio solicoita valores conocidos, pueden ser ingresados por el alumno por lo tanto;
+
+res = float(input("Indicar valor de resistencia en ohm"))
+inten = float(input["Indicar valor de intensidad en amper "])
+volt = inten * res 
+print[volt]
